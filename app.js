e177a1ca2cd8aef53d0abf10ac90a9b9caf49bb5
Srirangan <srirangan@gmail.com>
2012-04-26 15:22:53 +0530

    Remove layout, single page apps don't need layouts

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', {layout: false});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
